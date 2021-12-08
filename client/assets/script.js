8532438b45f0ee4ee14457ad3ad364d576728505
Julian James <julian@Julians-MacBook-Pro.local>
2021-12-08 15:45:47 +0000

    updated script.js

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,15 +1,21 @@
-const form = document.querySelector('form');
+const form = document.querySelector('#form');
 
+
+const getResults = async (genre) => {
+    const response = await fetch(`http://localhost:3000/genres/${genre}`)
+    const data = await response.json();
+    console.log(data);
+}
 
 const fetchAllMovies = (e) => {
     console.log('hi');
     e.preventDefault();
-    document.getElementById("logo").style.display = 'none';
-    // fetch('server/data.js')
-    //     .then( resp => resp.json() )
-    //     .then(document.getElementById('text'));
-    
-    
+    document.getElementById("homepage").style.display = 'none';
+    document.getElementById("results").style.display = 'block';
+    document.getElementById("stylesheet1").disabled = "disabled";
+    document.getElementById("stylesheet2").disabled = undefined;
+    const genre = e.target.gsearch.value;
+    getResults(genre);
 };
 
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const movieRoutes = require("./controllers/genre");
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 app.get("/", (req, res) => {
     res.send("Welcome to movie search");
@@ -12,3 +13,5 @@ app.use("/genres", movieRoutes);
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
+
+app.use(cors());
