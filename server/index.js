bea17d081e799aa718e27c68baebd9414425fa6c
Julian James <julian@Julians-MacBook-Pro.local>
2021-12-10 12:50:40 +0000

    added Procfile heroku file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const movieRoutes = require("./controllers/genre");
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000
 const cors = require('cors');
 
 app.get("/", (req, res) => {
